78c344d88127c65826315d2d816315b1e06b4737
mmol1na <mm0l1n4b@gmail.com>
2024-04-01 11:26:53 -0300

    use oxocarbon

--- init.lua
+++ init.lua
@@ -848,7 +848,7 @@ require('lazy').setup({
 
 -- NOTE: THEME SETTING
 
-vim.cmd 'colorscheme modus'
+vim.cmd 'colorscheme oxocarbon'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
