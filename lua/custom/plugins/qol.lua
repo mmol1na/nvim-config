be5b3e2d4bc78d7431430962d58764d88aa2c8c3
Martín Molina B. <mm0l1n4b@gmail.com>
2024-04-14 23:12:20 -0400

    more simplification

--- lua/custom/plugins/qol.lua
+++ lua/custom/plugins/qol.lua
@@ -15,4 +15,10 @@ return {
       }
     end,
   },
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 }
