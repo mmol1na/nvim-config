8fa0a77da23254eb0884f7019b07d6ed9bb2044f
mmol1na <mm0l1n4b@gmail.com>
2024-04-15 09:21:30 -0400

    remove modus themes and remove gruvbox config

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,61 +2,12 @@ return {
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
-    config = function()
-      -- Default options:
-      require('gruvbox').setup {
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = '', -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      }
-    end,
   },
   {
     'nyoom-engineering/oxocarbon.nvim',
     priority = 1000,
     config = function()
       vim.cmd 'colorscheme oxocarbon'
-    end,
-  },
-  {
-    'miikanissi/modus-themes.nvim',
-    priority = 1000,
-    config = function()
-      require('modus-themes').setup {
-        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-        -- `auto` will automatically set style based on background set with vim.o.background
-        style = 'auto',
-        variant = 'default', -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-        transparent = false, -- Transparent background (as supported by the terminal)
-        dim_inactive = false, -- "non-current" windows are dimmed
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-        },
-      }
     end,
   },
 }
