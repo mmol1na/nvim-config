96cbe75eda589a5a6c6f4237ad9ea1d0b2a4a00b
mmol1na <mm0l1n4b@gmail.com>
2024-04-23 01:51:53 -0400

    Add themes and stuff

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,12 +2,31 @@ return {
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
+    config = function() end,
   },
   {
     'nyoom-engineering/oxocarbon.nvim',
     priority = 1000,
+    config = function() end,
+  },
+  {
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd 'colorscheme oxocarbon'
+      -- configs go here
+    end,
+  },
+  {
+    'comfysage/evergarden',
+    opts = {
+      transparent_background = false,
+      contrast_dark = 'medium', -- 'hard'|'medium'|'soft'
+      overrides = {}, -- add custom overrides
+    },
+    config = function()
+      vim.cmd 'colorscheme evergarden'
     end,
   },
 }
