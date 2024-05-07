e31989726b8c2394ca73188503816061bd896d51
Martín Molina B. <mm0l1n4b@gmail.com>
2024-05-07 17:24:10 -0400

    delete a comment

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -5,7 +5,7 @@ return {
     version = '*',
     dependencies = {
       'SmiteshP/nvim-navic',
-      'nvim-tree/nvim-web-devicons', -- optional dependency
+      'nvim-tree/nvim-web-devicons',
     },
     opts = {
       -- configurations go here
